cadae5926910c9098f3f1f0afe89949740b578c8
SeeBenProgram <ben@seebenprogram.com>
2017-09-19 17:43:51 -0400

    Add function to get Google APIs geolocation data

--- main.js
+++ main.js
@@ -16,4 +16,26 @@ if (navigator.geolocation) {
 
 	});
 
+	// A function that takes latitude and longitude, retrieves location using Google Maps API
+	function cityState(lat, lon) {
+
+		// Generate URL specific to geolocation
+		var mapAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&sensor=false";
+
+		// Ajax Request to Google Maps Geolocation API
+		var apiRequest = new XMLHttpRequest();
+		apiRequest.open('GET', mapAPI);
+
+		apiRequest.onload = function () {
+			var locationData = JSON.parse(apiRequest.responseText);
+
+			// Call function to render 'city, state' to DOM
+			renderCityState(locationData);
+
+		};
+
+		apiRequest.send();
+
+	}; // End function cityState()
+
 }; // End 'if (navigator.geolocation)'
